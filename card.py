cc0cf38a6dca4f09c68bbf5ba21553c6ddd511ba
HurtBetter <ethanyhua@gmail.com>
2024-11-23 17:08:38 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,7 +1,7 @@
 class Card():
     # these unicode code points are the suits.
     SUITS_HUMAN = [u"\u2660", u"\u2665", u"\u2666", u"\u2663"]
-    FACES_HUMAN = ['A'] + range(2, 11) + ['J', 'Q', 'K']
+    FACES_HUMAN = ['A'] + [i for i in range(2, 11)] + ['J', 'Q', 'K']
 
     SUITS = range(0, 4)
     FACES = range(0, 13)
@@ -19,7 +19,3 @@ class Card():
 
     def __unicode__(self):
         return u"%s%s" % (self.FACES_HUMAN[self.face], self.SUITS_HUMAN[self.suit])
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
