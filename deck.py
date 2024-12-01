3aa90493ced3e454881a0ccaf1460f0ffb2b32a9
HurtBetter <ethanyhua@gmail.com>
2024-12-01 10:35:26 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,23 +1,32 @@
-import random
+"""
+The deck class makes the deck used for the game. This class was edited very little by us,
+but nortably we added a deckAmount instance variable that is the amount of decks used in the game
+"""
 
+
+import random
 from card import Card
 
+
 class Deck():
-    def __init__(self):
-        self.cards = []
+   def __init__(self, deckAmount):
+       self.cards = []
+       self.deckAmount = deckAmount
+       for i in range (deckAmount):
+           for suit in Card.SUITS:
+               for face in Card.FACES:
+                   self.cards.append(Card(suit, face))
 
-        for suit in Card.SUITS:
-            for face in Card.FACES:
-                self.cards.append(Card(suit, face))
 
-        random.shuffle(self.cards)
+       random.shuffle(self.cards)
 
-    def draw(self):
-        if self.cards:
-            return self.cards.pop()
-        # reshuffle.
-        newdeck = Deck()
 
-        self.cards = newdeck.cards
-        return self.draw()
+   def draw(self):
+       if self.cards:
+           return self.cards.pop()
+       # reshuffle.
+       newdeck = Deck(self.deckAmount)
 
+
+       self.cards = newdeck.cards
+       return self.draw()
