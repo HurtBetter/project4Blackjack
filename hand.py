fd7c4e06f603a4f2cf8429023382ae9e0b8b36a3
HurtBetter <ethanyhua@gmail.com>
2024-12-01 10:36:00 -0500

    Update hand.py

--- hand.py
+++ hand.py
@@ -1,31 +1,44 @@
+"""
+The hand class represents the hand that a player/dealer has.
+"""
+
+
 class Hand:
-    def __init__(self):
-        self.cards = []
+   def __init__(self):
+       self.cards = []
 
-    def is_soft(self):
-        value = 0
-        has_ace = False
 
-        for card in self.cards:
-            if card.is_ace():
-                has_ace = True
+   def is_soft(self):
+       value = 0
+       has_ace = False
 
-            value += card.value()
 
-        return has_ace and value <= 11
+       for card in self.cards:
+           if card.is_ace():
+               has_ace = True
 
-    def value(self):
-        value = 0
-        has_ace = False
 
-        for card in self.cards:
-            value += card.value()
+           value += card.value()
 
-            if card.is_ace():
-                has_ace = True
 
-        if has_ace and value <= 11:
-            value += 10
+       return has_ace and value <= 11
 
-        return value
 
+   def value(self):
+       value = 0
+       has_ace = False
+
+
+       for card in self.cards:
+           value += card.value()
+
+
+           if card.is_ace():
+               has_ace = True
+
+
+       if has_ace and value <= 11:
+           value += 10
+
+
+       return value
